10d7f5dd1005fb8bb4169ee5db8d3d4394d57775
Puthea Khem <puthea@web-essentials.asia>
2015-08-04 14:57:35 +0700

    [TASK] done with register and login

---
+++ Packages/Application/WE.Khmercourse/Classes/WE/Khmercourse/Domain/Repository/LoginRepository.php
@@ -0,0 +1,19 @@
+<?php
+namespace WE\Khmercourse\Domain\Repository;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "WE.Khmercourse".        *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Persistence\Repository;
+
+/**
+ * @Flow\Scope("singleton")
+ */
+class LoginRepository extends Repository {
+
+	// add customized methods here
+
+}
